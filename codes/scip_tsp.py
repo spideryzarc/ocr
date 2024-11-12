311f65b850968208ea867e7a77507193b99e1f51
spideryzarc <spideryzarc@gmail.com>
2024-11-12 17:40:35 -0300

    Enhance TSP ad-hoc subtours elimination; add plotting functionality, optimize constraint handling, and improve runtime reporting.

--- codes/scip_tsp.py
+++ codes/scip_tsp.py
@@ -1,9 +1,9 @@
+import time
 from pyscipopt import Model, SCIP_EVENTTYPE, SCIP_RESULT, Conshdlr, SCIP_PRESOLTIMING, SCIP_PROPTIMING
 from pyscipopt import quicksum as qsum
 import numpy as np
 import matplotlib.pyplot as plt
 from itertools import combinations as comb
-from itertools import product
 
 
 def tsp_mtz(c: np.array) -> tuple:
@@ -78,10 +78,13 @@ def tsp_all_subtours(c: np.array) -> tuple:
     return min_cost, tour
 
 
-def tsp_adhoc_subtours(c: np.array) -> tuple:
+def tsp_adhoc_subtours(c: np.array, plot=False, points=None, delay=0) -> tuple:
     '''
     solve TSP with ad-hoc subtours elimination constraints
     c: np.array - cost matrix
+    plot: bool - plot subtours while solving for didactic purposes
+    points: np.array - list of points needed for plotting
+    delay: int - delay in milliseconds for plotting
     return: tuple - (min_cost, tour)
     '''
     n = c.shape[0]
@@ -95,26 +98,49 @@ def tsp_adhoc_subtours(c: np.array) -> tuple:
         model.addCons(qsum(x[j, i] for j in range(n) if i != j) == 1)
     # remove verbose
     model.hideOutput()
-    run_time = 0
-    while True:
+    # optimize iteratively
+    start_time = time.time()  # start timing
+    unfeasible = True
+    if plot:
+        plt.ion()  # enable interactive mode
+    while unfeasible:
         model.optimize()
-        run_time += model.getSolvingTime()
-        tour = [0]
-        while True:
-            nxt = next(i for i in range(n) if tour[-1] != i and model.getVal(x[tour[-1], i]) > 0.5)
-            if nxt == 0:
+        visited = np.zeros(n, dtype=bool)  # visited nodes
+        tours = []  # list of subtours
+        # find subtours
+        for start in range(n):
+            if visited[start]:
+                continue
+            tour = [start]
+            nxt = next(i for i in range(n) if start != i and model.getVal(x[start, i]) > 0.5)
+            while nxt != start:
+                tour.append(nxt)
+                visited[nxt] = True
+                nxt = next(i for i in range(n) if tour[-1] != i and model.getVal(x[tour[-1], i]) > 0.5)
+            if len(tour) == n:
+                unfeasible = False
                 break
-            tour.append(nxt)
-        if len(tour) == n:
-            break
-        print("Subtour found:", tour)
-        # reset model
-        model.freeTransform()
-        # add subtour elimination constraint
-        model.addCons(qsum(x[i, j] for i in range(n) for j in range(n) if i != j and (i in tour) and (j in tour)) <= len(tour) - 1)
+            if len(tour) <= n//2:  # ignore subtours with more than n//2 nodes
+                tours.append(tour)
+        if unfeasible:
+            print("Subtours found: ", tours)
+            # add subtour elimination constraints
+            model.freeTransform()
+            for tour in tours:
+                model.addCons(qsum(x[i, j] for i in range(n) for j in range(n) if i != j and (i in tour) and (j in tour)) <= len(tour) - 1)
+            if plot:
+                # plot subtours
+                plot_tours(points, tours)
+                plt.pause(delay) # delay in seconds for plotting
+    if plot:
+        plt.ioff()  # disable interactive mode
+        plt.close() # close plot
+    end_time = time.time()  # end timing
     min_cost = model.getObjVal()
     # print running time
-    print("Running time: ", run_time)
+    print("Running time: ", end_time - start_time)
+    # print number of constraints
+    print("Number of constraints: ", model.getNConss())
     return min_cost, tour
 
 
@@ -126,6 +152,8 @@ def plot_tour(points: np.array, tour: np.array):
     n = points.shape[0]
     # add first point to the end
     tour = np.append(tour, tour[0])
+    # clear plot if exists
+    plt.close()    
     # create axis
     fig, ax = plt.subplots()
     # plot tour
@@ -135,6 +163,26 @@ def plot_tour(points: np.array, tour: np.array):
     for i in range(n):
         ax.annotate(str(i), (points[i, 0], points[i, 1]), color='white', weight='bold', fontsize=8, ha='center', va='center')
     plt.show()
+
+
+def plot_tours(points: np.array, tours: list):
+    '''
+    points: np.array - list of points
+    tours: list - list of subtours
+    '''
+    n = points.shape[0]
+    fig, ax = plot_tours.fig_ax
+    ax.clear()  # clear axis
+    # plot points
+    ax.plot(points[:, 0], points[:, 1], 'o', color='#624d6b', markersize=13, markeredgewidth=1, markeredgecolor='#202020')
+    for i in range(n):
+        ax.annotate(str(i), (points[i, 0], points[i, 1]), color='white', weight='bold', fontsize=8, ha='center', va='center')
+    # plot each subtour
+    for tour in tours:
+        tour = np.append(tour, tour[0])  # add first point to the end
+        ax.plot(points[tour, 0], points[tour, 1], linestyle='-', linewidth=2)
+    fig.canvas.draw_idle()  # update the plot without blocking
+plot_tours.fig_ax = plt.subplots() # create 'static' figure and axis for plot_tours function
 
 
 def make_random_instance(n: int = 10) -> tuple:
@@ -153,10 +201,10 @@ def make_random_instance(n: int = 10) -> tuple:
 
 
 if __name__ == "__main__":
-    points, c = make_random_instance(25)
+    points, c = make_random_instance(100)
     # min_cost, tour = tsp_mtz(c)
     # min_cost, tour = tsp_all_subtours(c)
-    min_cost, tour = tsp_adhoc_subtours(c)
+    min_cost, tour = tsp_adhoc_subtours(c, plot=True, points=points, delay=0.1)
     print("Min Cost:", min_cost)
 
     # plot tour
