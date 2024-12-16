90422d2f0fb347500d0339faa6d3296216259dea
spideryzarc <spideryzarc@gmail.com>
2024-12-16 17:41:38 -0300

    Refactor next_fit and best_fit_dec functions; remove unused parameters and streamline logic. Add local_search function for improved bin packing optimization.

--- codes/bpp.py
+++ codes/bpp.py
@@ -23,7 +23,7 @@ def first_fit_dec(w:np.array, C:int) -> tuple:
     return len(loads), loads, bins
     
 
-def next_fit(w:np.array, C:int, order_idx:list = None) -> tuple:
+def next_fit(w:np.array, C:int) -> tuple:
     '''
     w: np.array - list of weights of each item
     C: int - capacity of each bin
@@ -32,27 +32,26 @@ def next_fit(w:np.array, C:int, order_idx:list = None) -> tuple:
     n = len(w)
     bins = [[]]
     loads = [0]
-    if order_idx is None:
-        order_idx = range(n)
-    for i in order_idx:
-        w_i = w[i]
+    for w_i in w:
         if loads[-1] + w_i <= C:
             bins[-1].append(w_i)
             loads[-1] += w_i
         else:
             bins.append([w_i])
             loads.append(w_i)
-    return len(loads), loads, bins
+    return loads, bins
 
-def best_fit_dec(w:np.array, C:int) -> tuple:
+def best_fit_dec(w:np.array, C:int, sort:bool =True) -> tuple:
     '''
     w: np.array - list of weights of each item
     C: int - capacity of each bin
+    sort: bool - sort the weights in decreasing order
     return: tuple - (min_bins, bin_assignment)
     '''
     n = len(w)
     #sort the weights in decreasing order
-    w = np.sort(w)[::-1]
+    if sort:
+        w = np.sort(w)[::-1]
     bins = [[]]
     loads = [0]
     for w_i in w:
@@ -68,7 +67,7 @@ def best_fit_dec(w:np.array, C:int) -> tuple:
         else:
             bins.append([w_i])
             loads.append(w_i)
-    return len(loads), loads, bins
+    return loads, bins
 
 def worst_fit_dec(w:np.array, C:int) -> tuple:
     '''
@@ -97,21 +96,54 @@ def worst_fit_dec(w:np.array, C:int) -> tuple:
     return len(loads), loads, bins
 
 
+def local_search(w:np.array, C:int,sol:list) -> tuple:
+    '''
+    w: np.array - list of weights of each item
+    C: int - capacity of each bin
+    sol: list of list - bin assignment
+    return: tuple - (min_bins, bin_assignment)
+    '''
+    itens = np.concatenate(sol)
+    n_bins = len(sol)
+    loads = [np.sum(bin) for bin in sol]
+    sq_sum = np.sum(np.square(loads))
+    imp = True
+    while imp:
+        imp = False
+        for i in range(1,len(itens)):
+            for j in range(i):
+                itens[i], itens[j] = itens[j], itens[i] #swap
+                loads, bins = best_fit_dec(itens, C,False)
+                new_n_bins = len(loads)
+                if new_n_bins <= n_bins:
+                    new_sq_sum = np.sum(np.square(loads))
+                    if new_n_bins < n_bins or new_sq_sum > sq_sum:
+                        n_bins = new_n_bins
+                        sq_sum = new_sq_sum
+                        sol = bins
+                        imp = True
+                        print('new solution:', n_bins, sq_sum)
+                    else:
+                        itens[i], itens[j] = itens[j], itens[i] #swap back
+                else:
+                    itens[i], itens[j] = itens[j], itens[i] #swap back
+                    
+    return sol      
+                
+    
+
 if __name__ == "__main__":
-    n = 5000
+    # np.random.seed(7)
+    n = 200
     m = n
     C = 100
-    w = np.random.randint(1, 2*C//3, n)
-    # print("Weights:", w)
-    print(first_fit_dec(w, C)[0])
-    
-    order_idx = np.argsort(w)[::-1]
-    print (next_fit(w, C, order_idx)[0])
-    # best = np.inf
-    # for _ in range(100000):
-    #     sol = next_fit(w, C,np.random.permutation(n))
-    #     if sol[0] < best:
-    #         best = sol[0]
-    #         print(sol[0])
-    print(best_fit_dec(w, C)[0])
-    print(worst_fit_dec(w, C)[0])
+    while True:
+        w = np.random.randint(1, 2*C//3, n)
+        # print('input:', w)
+        _, sol = best_fit_dec(w, C)
+        
+        lb = int(np.ceil(np.sum(w)/C))
+        if len(sol) > lb:  
+            print('best_fit_dec:', len(sol), lb)
+            local_search(w, C,sol) #[[i] for i in w])
+            break
