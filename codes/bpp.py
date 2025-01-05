dd40f1cd8f9440bc8f1020949c70ab33e38637e7
spideryzarc <spideryzarc@gmail.com>
2025-01-05 16:38:46 -0300

    Refactor bin packing functions: rename and update parameters for clarity; enhance local_search function with improved item swapping logic and bin mapping.

--- codes/bpp.py
+++ codes/bpp.py
@@ -1,14 +1,17 @@
 import numpy as np
 
-def first_fit_dec(w:np.array, C:int) -> tuple:
+
+def first_fit(w: np.array, C: int, sort_dec: bool = True) -> tuple:
     '''
     w: np.array - list of weights of each item
     C: int - capacity of each bin
+    sort_dec: bool - sort the weights in decreasing order
     return: tuple - (min_bins, bin_assignment)
     '''
     n = len(w)
-    #sort the weights in decreasing order
-    w = np.sort(w)[::-1]
+    # sort the weights in decreasing order
+    if sort_dec:
+        w = np.sort(w)[::-1]
     bins = [[]]
     loads = [0]
     for w_i in w:
@@ -20,10 +23,10 @@ def first_fit_dec(w:np.array, C:int) -> tuple:
         else:
             bins.append([w_i])
             loads.append(w_i)
-    return len(loads), loads, bins
-    
+    return loads, bins
 
-def next_fit(w:np.array, C:int) -> tuple:
+
+def next_fit(w: np.array, C: int) -> tuple:
     '''
     w: np.array - list of weights of each item
     C: int - capacity of each bin
@@ -41,16 +44,17 @@ def next_fit(w:np.array, C:int) -> tuple:
             loads.append(w_i)
     return loads, bins
 
-def best_fit_dec(w:np.array, C:int, sort:bool =True) -> tuple:
+
+def best_fit(w: np.array, C: int, sort_dec: bool = True) -> tuple:
     '''
     w: np.array - list of weights of each item
     C: int - capacity of each bin
-    sort: bool - sort the weights in decreasing order
+    sort_dec: bool - sort the weights in decreasing order
     return: tuple - (min_bins, bin_assignment)
     '''
     n = len(w)
-    #sort the weights in decreasing order
-    if sort:
+    # sort the weights in decreasing order
+    if sort_dec:
         w = np.sort(w)[::-1]
     bins = [[]]
     loads = [0]
@@ -69,15 +73,18 @@ def best_fit_dec(w:np.array, C:int, sort:bool =True) -> tuple:
             loads.append(w_i)
     return loads, bins
 
-def worst_fit_dec(w:np.array, C:int) -> tuple:
+
+def worst_fit(w: np.array, C: int, sort_dec: bool = True) -> tuple:
     '''
     w: np.array - list of weights of each item
     C: int - capacity of each bin
+    sort_dec: bool - sort the weights in decreasing order
     return: tuple - (min_bins, bin_assignment)
     '''
     n = len(w)
-    #sort the weights in decreasing order
-    w = np.sort(w)[::-1]
+    # sort the weights in decreasing order
+    if sort_dec:
+        w = np.sort(w)[::-1]
     bins = [[]]
     loads = [0]
     for w_i in w:
@@ -93,44 +100,71 @@ def worst_fit_dec(w:np.array, C:int) -> tuple:
         else:
             bins.append([w_i])
             loads.append(w_i)
-    return len(loads), loads, bins
+    return loads, bins
 
 
-def local_search(w:np.array, C:int,sol:list) -> tuple:
-    '''
+def local_search(w: np.array, C: int, sol: list, lb: int = 0) -> tuple:
+    ''' try to improve the solution by local search. The solution is flattened and the items are swapped, best_fit is used to decode the permutation to bins.
     w: np.array - list of weights of each item
     C: int - capacity of each bin
     sol: list of list - bin assignment
+    lb: int - lower bound of the number of bins
     return: tuple - (min_bins, bin_assignment)
     '''
-    itens = np.concatenate(sol)
-    n_bins = len(sol)
-    loads = [np.sum(bin) for bin in sol]
-    sq_sum = np.sum(np.square(loads))
-    imp = True
-    while imp:
+    itens = np.concatenate(sol)  # flatten the solution
+    n_bins = len(sol)  # number of bins
+    loads = [np.sum(bin) for bin in sol]  # loads of each bin
+    sq_sum = np.sum(np.square(loads))  # sum of the square of the loads
+    imp = True  # flag to indicate if the solution was improved
+    # map each item to its bin
+    bin_map = np.empty(len(itens), dtype=int)
+    j = 0
+    for i, bin in enumerate(sol):
+        bin_map[j:j+len(bin)] = i
+        j = i + len(bin)
+    while imp:  # while the solution is improved
         imp = False
-        for i in range(1,len(itens)):
-            for j in range(i):
-                itens[i], itens[j] = itens[j], itens[i] #swap
-                loads, bins = best_fit_dec(itens, C,False)
-                new_n_bins = len(loads)
-                if new_n_bins <= n_bins:
-                    new_sq_sum = np.sum(np.square(loads))
-                    if new_n_bins < n_bins or new_sq_sum > sq_sum:
+        for i in range(len(itens)-1,-1,-1):
+            if imp:
+                break
+            for j in range(i+1, len(itens)):
+                # skip if the items are in the same bin or are the same item (weights are equal)
+                if bin_map[i] == bin_map[j] or itens[i] == itens[j]:
+                    continue
+                itens[i], itens[j] = itens[j], itens[i]  # swap
+                # decode permutation to bins using best_fit
+                loads, bins = best_fit(itens, C, sort_dec=False)
+                # loads, bins = first_fit(itens, C, sort_dec=False)
+                # loads, bins = worst_fit(itens, C, sort_dec=False)
+                # loads, bins = next_fit(itens, C)
+                new_n_bins = len(loads)  # number of bins
+                if new_n_bins <= n_bins:  # first criteria to accept the new solution
+                    new_sq_sum = np.sum(np.square(loads))  # sum of the square of the loads
+                    if new_n_bins < n_bins or new_sq_sum > sq_sum:  # second criteria to accept the new solution
+                        imp = True
+                        # update the solution
                         n_bins = new_n_bins
                         sq_sum = new_sq_sum
                         sol = bins
-                        imp = True
+                        # update flatten solution
+                        itens = np.concatenate(sol)
+                        # update map
+                        k = 0
+                        for l, bin in enumerate(sol):
+                            bin_map[k:k+len(bin)] = l
+                            k = l + len(bin)
                         print('new solution:', n_bins, sq_sum)
+                        # print([int(l) for l in loads])
+                        if n_bins == lb:
+                            return sol
+                        break # break j loop
                     else:
-                        itens[i], itens[j] = itens[j], itens[i] #swap back
+                        itens[i], itens[j] = itens[j], itens[i]  # swap back
                 else:
-                    itens[i], itens[j] = itens[j], itens[i] #swap back
-                    
-    return sol      
-                
-    
+                    itens[i], itens[j] = itens[j], itens[i]  # swap back
+
+    return sol
+
 
 if __name__ == "__main__":
     # np.random.seed(7)
@@ -140,10 +174,10 @@ if __name__ == "__main__":
     while True:
         w = np.random.randint(1, 2*C//3, n)
         # print('input:', w)
-        _, sol = best_fit_dec(w, C)
-        
+        _, sol = best_fit(w, C)
+
         lb = int(np.ceil(np.sum(w)/C))
-        if len(sol) > lb:  
+        if len(sol) > lb:
             print('best_fit_dec:', len(sol), lb)
-            local_search(w, C,sol) #[[i] for i in w])
+            local_search(w, C, sol, lb)
             break
