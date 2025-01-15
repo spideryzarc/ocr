cdf265a6cd1865dcf1a540ce5879956bf37fd422
spideryzarc <spideryzarc@gmail.com>
2025-01-15 17:51:58 -0300

    Implement VND algorithm with 2-opt and or-opt moves for TSP; enhance tour improvement methods and update main execution flow.

--- codes/tsp.py
+++ codes/tsp.py
@@ -50,31 +50,111 @@ def furthest_order(c:np.array)->list:
         dist = np.minimum(dist, c[k])
         dist[k] = -1
     return order
+
+def or_opt(c:np.array, tour:list)->bool:
+    ''' search for a improving move in the tour using or-opt
+    c: cost matrix
+    tour: current tour to be improved, will be modified in place
+    return: True if an improving move is found, False otherwise
+    '''
+    n = len(c)
+    if __debug__: custo_ini = cost(c, tour) # initial cost for debug only
+    for i in range(n):
+        ant_i = i-1
+        post_i = i+1 if i < n-1 else 0
+        rem_delta =  c[tour[ant_i],tour[post_i]] \
+                    -c[tour[ant_i],tour[i]] - c[tour[i],tour[post_i]]
+        for j in range(n):
+            if j in (i, post_i): continue # skip i, i-1 and i+1 
+            ant_j = j-1
+            add_delta = -c[tour[ant_j],tour[j]] \
+                        +c[tour[ant_j],tour[i]] + c[tour[i],tour[j]]
+            if add_delta + rem_delta < -1e-6:
+                if i < j:
+                    tour.insert(j, tour[i])
+                    tour.pop(i)
+                else:
+                    vi = tour.pop(i)
+                    tour.insert(j, vi)
     
+                if __debug__: 
+                    print("OR-opt move:", tour[i], tour[j])
+                    delta = cost(c, tour) - custo_ini # for debug only
+                    if not np.isclose(delta, add_delta + rem_delta):
+                        raise ValueError("OR-opt move error")
+                return True
+    return False
+
+def two_opt(c:np.array, tour:list)->bool:
+    ''' search for a improving move in the tour using 2-opt
+    c: cost matrix
+    tour: current tour to be improved, will be modified in place
+    return: True if an improving move is found, False otherwise
+    '''
+    n = len(c)
+    if __debug__: custo_ini = cost(c, tour) # initial cost for debug only
+    for i in range(n):
+        for j in range(i+2, n-(i==0)): # i+2 to avoid adjacent edges and n-1 if i==0
+            delta = c[tour[i],tour[j]] + c[tour[i+1],tour[(j+1)%n]] \
+                    -c[tour[i],tour[i+1]] - c[tour[j],tour[(j+1)%n]]
+            if delta < -1e-6:
+                if j == n-1:
+                    tour[i+1:] = tour[i+1:][::-1]
+                    print('opa')
+                else:
+                    tour[i+1:j+1] = tour[j:i:-1]
+                if __debug__: 
+                    print("2-opt move:", i, j)
+                    delta_real = cost(c, tour) - custo_ini # for debug only
+                    if not np.isclose(delta, delta_real):
+                        raise ValueError("2-opt move error")
+                return True
+    return False
+                    
+
+def VND(c:np.array, tour:list, points)->None:
+    ''' perform a VND in the tour
+    c: cost matrix
+    tour: current tour to be improved, will be modified in place
+    points: coordinates of the points
+    '''
+    while True:
+        if two_opt(c, tour):
+            continue
+        if or_opt(c, tour):
+            continue
+        break
 
 if __name__ == "__main__":
+    np.random.seed(42)
     points, c = make_random_instance(100)
    
     nn_tour = nearest_neighbor(c)
     print ("NN Tour Cost:", cost(c, nn_tour))
     plot_tour(points, nn_tour)
+    VND(c, nn_tour, points)
+    print ("NN Tour Cost:", cost(c, nn_tour))
+    plot_tour(points, nn_tour)
     
-    order  = furthest_order(c)
-    futher_tour = best_insertion(c, order, points)
-    print("Furthest Order Cost:", cost(c, futher_tour))
-    plot_tour(points, futher_tour)
     
-    best = cost(c, nn_tour)
-    for _ in range(100):
-        np.random.shuffle(order)
-        bi_tour = best_insertion(c, order, points)
-        a = cost(c, bi_tour)
-        if a < best:
-            best = a
-            print("BI Tour Cost:", a)
-            plot_tour(points, bi_tour)
+    # order  = furthest_order(c)
+    # futher_tour = best_insertion(c, order, points)
+    # print("Furthest Order Cost:", cost(c, futher_tour))
+    # plot_tour(points, futher_tour)
+    
+    # best_cost = cost(c, nn_tour)
+    # best_tour = nn_tour
+    # for _ in range(100):
+    #     np.random.shuffle(order)
+    #     bi_tour = best_insertion(c, order, points)
+    #     a = cost(c, bi_tour)
+    #     if a < best_cost:
+    #         best_cost = a
+    #         best_tour = bi_tour
+    #         print("BI Tour Cost:", a)
+    # plot_tour(points, bi_tour)
             
     
-    opt_cost, opt_tour = tsp_adhoc_subtours(c, plot=False, points=points)
-    print("Optimal Cost:", opt_cost)
-    plot_tour(points, opt_tour)
+    # opt_cost, opt_tour = tsp_adhoc_subtours(c, plot=False, points=points)
+    # print("Optimal Cost:", opt_cost)
+    # plot_tour(points, opt_tour)
